3be343d1923ebd194c279ba79c945911d47609e2
Felipe Ferreira <78969308+felipefrx@users.noreply.github.com>
2024-07-11 14:57:57 -0300

    Update enable_prometheus.sh

--- rabbitmq/enable_prometheus.sh
+++ rabbitmq/enable_prometheus.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 set -e
 
-# Habilitar o plugin do Prometheus
 rabbitmq-plugins enable --offline rabbitmq_prometheus
 
-# Reiniciar o serviço do RabbitMQ para aplicar as alterações
 service rabbitmq-server restart
